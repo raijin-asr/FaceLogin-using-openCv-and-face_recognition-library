cddd77220d0278fc777f4729a699027644c6ce4f
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2024-04-18 18:30:17 +0545

    installed Cmake, issue in installing face_recognition library

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import cv2
 import face_recognition
+
+img = cv2.imread("images/known/messi.jpg")
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+img = cv2.imread("images/unknown/messi1.jpg")
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+result = face_recognition.compare_faces([img_encoding], img_encoding2)
+print("Result: ", result)
